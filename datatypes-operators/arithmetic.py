f14c0e5441d43cf0d6f60cd5f2edea3d694a7624
Dave McCollough <davemccollough@gmail.com>
2021-12-12 16:40:36 -0600

    Arthmetic

---
+++ datatypes-operators/arithmetic.py
@@ -0,0 +1,32 @@
+# Addition
+# +
+print(5 + 5)  # Returns 10
+print(5 + 5 + 5)  # Returns 15
+
+# Subtraction
+# - 
+print(12 - 5) # Returns 7
+
+# Multiplication
+# *
+print(3 * 4)  # Returns 12
+
+# Division
+# / or //
+print(12 / 6) # Returns 2.0
+# To divide and round down to the nearest integer
+print(12 // 6) # Returns 2
+
+# Mod
+# %
+# Use to find the remainder after dividing
+print(3 % 4) # Returns 1
+
+# Exponentiation
+print(3 ** 4) # 81
+
+# Order of operations
+# Python follows mathmatical order of operations
+print(2 + 5 + 3 * 7) # Returns 28
+# If you want addition to happen first, enclose in parantheses
+print((2 + 5 + 3) * 7) # Returns 70
